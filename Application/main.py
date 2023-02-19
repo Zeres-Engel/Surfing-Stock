aa5f4b5d986c75b294f894eb80e8de9a6d6fff02
Nguyễn Phước Thành <93478665+Zeres-Engel@users.noreply.github.com>
2023-02-19 18:35:26 +0700

    requirement.txt

--- Application/main.py
+++ Application/main.py
@@ -5,19 +5,23 @@ from gui.core.json_settings import Settings
 from gui.uis.windows.main_window import *
 from gui.widgets import *
 from gui.uis.windows.main_window.functions_main_window import *
+
 os.environ["QT_FONT_DPI"] = "96"
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = UI_MainWindow()
-        self.ui.setup_ui(self) 
+        self.ui.setup_ui(self)
         settings = Settings()
         self.settings = settings.items
-        self.hide_grips = True 
+        self.hide_grips = True
         SetupMainWindow.setup_gui(self)
         self.show()
+
     def btn_clicked(self):
-        btn = SetupMainWindow.setup_btns(self)   
+        btn = SetupMainWindow.setup_btns(self)
         if btn.objectName() == "btn_FPT":
             self.ui.left_menu.select_only_one(btn.objectName())
             MainFunctions.set_page(self, self.ui.load_pages.page_1)
@@ -28,13 +32,18 @@ class MainWindow(QMainWindow):
             self.ui.left_menu.select_only_one(btn.objectName())
             MainFunctions.set_page(self, self.ui.load_pages.page_3)
         print(f"Button {btn.objectName()}, clicked!")
+
     def btn_released(self):
         btn = SetupMainWindow.setup_btns(self)
         print(f"Button {btn.objectName()}, released!")
+
     def resizeEvent(self, event):
         SetupMainWindow.resize_grips(self)
+
     def mousePressEvent(self, event):
         self.dragPos = event.globalPos()
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon("icon.ico"))
